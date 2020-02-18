09c408ec182ee96de404906318c04e5ffc262c08
tomsarpo <t.j.sarpola@gmail.com>
2020-02-19 00:11:08 +0200

    t13 done
    t13 done

--- t13-todolist/src/App.js
+++ t13-todolist/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todolist from './Todolist';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <link rel="stylesheet" 
+      href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" 
+      integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" 
+      crossOrigin="anonymous"></link>
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      
+      <div >
+        <h1 >TodoList</h1>
+        <Todolist/>
+      </div>
+      
     </div>
+    </>
   );
 }
 
--- t13-todolist/src/Todolist.js
+++ t13-todolist/src/Todolist.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 function TodoRow({ todo }) {
     return (
-            <tr>
+            <>
             <td key={todo.index}>{todo.index}</td><td>{todo.desc}</td><td>{todo.date}</td>
-            </tr>
+            </>
     );
 }
 
@@ -13,21 +13,28 @@ const Todolist = () => {
     const [date, setDate] = useState('');
     const [todos, setTodos] = useState([
         {
+            index: 0,
             desc: "Do something",
             date: "2020-02-19"
         },
         {
+            index: 1,
             desc: "Do something new",
             date: "2020-02-19"
         },
         {
+            index: 2,
             desc: "Do more",
             date: "2020-02-19"
         }
     ]);
 
     const addTodo = (desc, date) => {
-        const newTodos = [...todos, { desc, date }];
+        var max_id = Math.max.apply(Math, todos.map(function(o) { return o.index; }));
+        //var max_id = todos.reduce((prev, current) => (prev.index > current.index) ? prev : current, 1)
+        max_id = max_id + 1;
+        console.log("new todo: "+max_id, desc, date );
+        const newTodos = [...todos, { max_id, desc, date }];
         setTodos(newTodos);
     };
 
@@ -36,6 +43,13 @@ const Todolist = () => {
         event.preventDefault();
         if (!desc) return;
         addTodo(desc, date);
+    };
+
+    const handleDelete = index => {
+        console.log("delete:" + index);
+        const newTodos = todos.filter((todo, i) => i !== index);
+        console.log(newTodos);
+        setTodos(newTodos);
     }
 
     const inputChanged = (event) => {
@@ -47,17 +61,26 @@ const Todolist = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={inputChanged} value={desc}/>
+        <div >
+            <form className="pure-form " onSubmit={handleSubmit}><fieldset>
+                <legend>Add a todo item:</legend>
+                Description: 
+                <input type="text" placeholder="Description" onChange={inputChanged} value={desc}/>
+                Date: 
                 <input type="date" onChange={dateChanged} value={date}/>
-                <input type="submit" value="Add"/>
-            </form>
-            <table>
-            <tbody>
-                {todos.map((todo, index) => (
+                <button type="submit" className="pure-button pure-button-primary">Add</button>
+            </fieldset></form>
+            <table className="pure-table pure-table-horizontal">
+                <thead>
+                    <tr><th>Id</th><th>Description</th><th>Date</th><th>Actions</th></tr> 
+                </thead>
+                <tbody>                  
+                    {todos.map((todo, index) => (
+                        <tr>
                             <TodoRow todo={todo} index={index} key={index} />
-                        ))}
+                            <td><button onClick={() => handleDelete(todo.index)} className="pure-button pure-button-primary">Delete</button></td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
